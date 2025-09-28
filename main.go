cf079e6c46182366fa48475a8050954265715e91
thordoor <thordoor89@hotmail.com>
2025-09-28 20:28:50 +0200

    feat: added json response and route

--- main.go
+++ main.go
@@ -1,100 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
-	"net/url"
-	"time"
+
+	"github.com/throwpedro/ground_team_training_scanner/routes"
 )
-
-const (
-	BaseURL                 = "https://ground.brpsystems.com/brponline/api/ver3/apps/390/groupactivities"
-	Facility_GroenloekkeVej = "4"
-)
-
-type Dates struct {
-	Start string
-	End   string
-}
-
-type LocationData struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-type DurationData struct {
-	End   string `json:"end"`
-	Start string `json:"start"`
-}
-
-type ReturnData struct {
-	ID        int            `json:"id"`
-	Name      string         `json:"name"`
-	Duration  DurationData   `json:"duration"`
-	Locations []LocationData `json:"locations"`
-}
-
-func Fetch(link string) ([]ReturnData, error) {
-	res, err := http.Get(link)
-	if err != nil {
-		println(err)
-	}
-
-	var data []ReturnData
-	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
-		return nil, err
-	}
-	return data, nil
-}
 
 func main() {
-	baseUrl := "https://ground.brpsystems.com/brponline/api/ver3/apps/390/groupactivities"
-	url, err := url.Parse(baseUrl)
-	if err != nil {
-		println(err)
+	http.HandleFunc("/", routes.GetGroundTimes)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Failed to start server: ", err)
 	}
-
-	dates := BuildWeekDates()
-
-	q := url.Query()
-	q.Set("businessUnit", Facility_GroenloekkeVej)
-	q.Set("groupActivitiesFor", "web")
-	q.Set("period.end", dates.End)
-	q.Set("period.start", dates.Start)
-	q.Set("webCategory", "2")
-	url.RawQuery = q.Encode()
-
-	classes, err := Fetch(url.String())
-	if err != nil {
-		panic(err)
-	}
-
-	for _, c := range classes {
-		if c.Locations[0].Name != "Funktionelt Område" {
-			continue
-		}
-		fmt.Printf("ID: %d | Name: %s | Place: %s | Start: %s | End: %s\n",
-			c.ID, c.Name, c.Locations[0].Name, c.Duration.Start, c.Duration.End)
-	}
-}
-
-func BuildWeekDates() Dates {
-	now := time.Now().UTC()
-
-	start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
-
-	end := start.AddDate(0, 0, 6).
-		Add(time.Hour*23 + time.Minute*59 + time.Second*59 + time.Millisecond*999)
-
-	layout := "2006-01-02T15:04:05.000Z"
-	periodStart := start.Format(layout)
-	periodEnd := end.Format(layout)
-
-	returnVal := Dates{
-		Start: periodStart,
-		End:   periodEnd,
-	}
-
-	return returnVal
 }
---
+++ routes/routes.go
@@ -0,0 +1,121 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	BaseURL                 = "https://ground.brpsystems.com/brponline/api/ver3/apps/390/groupactivities"
+	Facility_GroenloekkeVej = "4"
+)
+
+type Dates struct {
+	Start string
+	End   string
+}
+
+type LocationData struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type DurationData struct {
+	End   string `json:"end"`
+	Start string `json:"start"`
+}
+
+type ReturnData struct {
+	ID        int            `json:"id"`
+	Name      string         `json:"name"`
+	Duration  DurationData   `json:"duration"`
+	Locations []LocationData `json:"locations"`
+}
+
+type ResponseData struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	StartTime string `json:"start_time"`
+	EndTime   string `json:"end_time"`
+	Area      string `json:"area"`
+}
+
+func GetGroundTimes(w http.ResponseWriter, r *http.Request) {
+	url, err := url.Parse(BaseURL)
+	if err != nil {
+		println(err)
+	}
+
+	buildQuery(url)
+
+	fitnessClasses, err := Fetch(url.String())
+	if err != nil {
+		panic(err)
+	}
+
+	var responseData []ResponseData
+
+	for _, c := range fitnessClasses {
+		if c.Locations[0].Name == "Funktionelt Område" {
+			responseData = append(responseData, ResponseData{
+				ID:        c.ID,
+				Name:      c.Name,
+				Area:      c.Locations[0].Name,
+				StartTime: c.Duration.Start,
+				EndTime:   c.Duration.End,
+			})
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(responseData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func Fetch(link string) ([]ReturnData, error) {
+	res, err := http.Get(link)
+	if err != nil {
+		println(err)
+	}
+
+	var data []ReturnData
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func BuildWeekDates() Dates {
+	now := time.Now().UTC()
+
+	start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+
+	end := start.AddDate(0, 0, 6).
+		Add(time.Hour*23 + time.Minute*59 + time.Second*59 + time.Millisecond*999)
+
+	layout := "2006-01-02T15:04:05.000Z"
+	periodStart := start.Format(layout)
+	periodEnd := end.Format(layout)
+
+	returnVal := Dates{
+		Start: periodStart,
+		End:   periodEnd,
+	}
+
+	return returnVal
+}
+
+func buildQuery(url *url.URL) {
+	dates := BuildWeekDates()
+
+	q := url.Query()
+	q.Set("businessUnit", Facility_GroenloekkeVej)
+	q.Set("groupActivitiesFor", "web")
+	q.Set("period.end", dates.End)
+	q.Set("period.start", dates.Start)
+	q.Set("webCategory", "2")
+	url.RawQuery = q.Encode()
+}
